163c48f74517900a16aa869791cb0599018b2675
Teddy <teddyrobbins3@gmail.com>
2020-11-05 16:14:36 -0500

    Made edits per Ethan's suggestions 11-5

--- airtable_script.js
+++ airtable_script.js
@@ -5,10 +5,6 @@ Edited 11-4-20
 Ethan Danahy and Teddy Robbins
 */
 
-window.addEventListener('load', function () {
-        setup();
-});
-
 //Wrapper functions for updating airtable based on button, slider, and text input
 function button_function(elem) {
     alert('Set airtable attribute "' + elem.getAttribute('airtable_value') + '" to be "' + elem.innerHTML + '"');
@@ -20,7 +16,6 @@ function text_function(elem){
     alert('Set airtable attribute "' + elem.getAttribute('airtable_value') + '" to be "' + elem.value + '"');
 }
 
-
 //Setup for assigning elements to wrapper functions
 function setup() {
     d = document.querySelectorAll("button");
@@ -31,8 +26,18 @@ function setup() {
     for (i=0; i<d.length; i++) {
         d[i].onclick = function () { range_function(this); }
     }
-    d = document.querySelectorAll("input[type=text]");
+    d = document.querySelectorAll("form");
     for (i=0; i<d.length; i++) {
-        d[i].onchange = function () { text_function(this); }
+        
+        var current_form = d[i];
+        var text_entry = d[i].elements[0];
+        var submit_button = d[i].elements[1];
+
+        submit_button.onclick = function () { text_function(text_entry); }
+
     }
 }
+
+window.addEventListener('load', function () {
+        setup();
+});
